b046a77431209cb4b28081011a679ba8a6368980
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-01-17 09:50:08 +0700

    add : content Handler in entity content

--- features/content/entity.go
+++ features/content/entity.go
@@ -1,8 +1,18 @@
 package content
 
+import "github.com/labstack/echo/v4"
+
 type Core struct {
 	ID      uint
 	Image   string
 	Caption string
 	UserID  uint
+}
+
+type ContentHandler interface {
+	Add() echo.HandlerFunc
+	ContentList() echo.HandlerFunc
+	MyContent() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Delete() echo.HandlerFunc
 }
