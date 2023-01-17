635c9eec4277226147e8fa800a90ce647b72304d
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-01-17 14:12:45 +0700

    feat : function Add and Response in Handler

--- features/content/handler/response.go
+++ features/content/handler/response.go
@@ -1,1 +1,23 @@
 package handler
+
+import "incrediblefour/features/content"
+
+type ContentResponse struct {
+	ID       uint   `json:"id"`
+	Avatar   string `json:"avatar"`
+	Username string `json:"username"`
+	Image    string `json:"image"`
+	Caption  string `json:"caption"`
+	UserID   uint   `json:"user_id"`
+}
+
+func ToResponse(data content.Core) ContentResponse {
+	return ContentResponse{
+		ID:       data.ID,
+		Avatar:   data.Avatar,
+		Username: data.Username,
+		Image:    data.Image,
+		Caption:  data.Caption,
+		UserID:   data.UserID,
+	}
+}
