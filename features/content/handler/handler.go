a87a7329eadfd98fcf27216dfb70052bd9782b9b
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-01-17 14:12:45 +0700

    feat : function Add and Response in Handler

--- features/content/handler/handler.go
+++ features/content/handler/handler.go
@@ -1,6 +1,12 @@
 package handler
 
-import "incrediblefour/features/content"
+import (
+	"incrediblefour/features/content"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 type contentHandle struct {
 	srv content.ContentService
@@ -9,5 +15,23 @@ type contentHandle struct {
 func New(cs content.ContentService) content.ContentHandler {
 	return &contentHandle{
 		srv: cs,
+	}
+}
+
+func (ch *contentHandle) Add() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		input := AddContentRequest{}
+		if err := c.Bind(&input); err != nil {
+			return c.JSON(http.StatusBadRequest, "invalid input")
+		}
+
+		cnv := ToCore(input)
+
+		res, err := ch.srv.Add(c.Get("user"), *cnv)
+		if err != nil {
+			log.Println("error post content : ", err.Error())
+			return c.JSON(http.StatusInternalServerError, "unable to process the data")
+		}
+		return c.JSON(http.StatusCreated, ToResponse(res))
 	}
 }
--- features/content/handler/request.go
+++ features/content/handler/request.go
@@ -1,1 +1,23 @@
 package handler
+
+import "incrediblefour/features/content"
+
+type AddContentRequest struct {
+	Image   string `json:"image"`
+	Caption string `json:"caption"`
+}
+
+func ToCore(data interface{}) *content.Core {
+	res := content.Core{}
+
+	switch data.(type) {
+	case AddContentRequest:
+		cnv := data.(AddContentRequest)
+		res.Image = cnv.Image
+		res.Caption = cnv.Caption
+	default:
+		return nil
+	}
+
+	return &res
+}
