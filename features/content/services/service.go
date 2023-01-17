172176aa2656784958ae9568d5e6fdea5254f2b9
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-01-17 13:16:45 +0700

    feat: function Add in Service

--- features/content/services/service.go
+++ features/content/services/service.go
@@ -1,7 +1,11 @@
 package services
 
 import (
+	"errors"
 	"incrediblefour/features/content"
+	"incrediblefour/helper"
+	"log"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -13,7 +17,37 @@ type contentSrv struct {
 
 func New(c content.ContentData) content.ContentService {
 	return &contentSrv{
-		data: d,
+		data: c,
 		vld:  validator.New(),
 	}
+}
+
+func (cs *contentSrv) Add(token interface{}, newContent content.Core) (content.Core, error) {
+	userID := helper.ExtractToken(token)
+
+	if userID <= 0 {
+		return content.Core{}, errors.New("user not found")
+	}
+
+	err := cs.vld.Struct(newContent)
+	if err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			log.Println(err)
+		}
+		return content.Core{}, errors.New("invalid input")
+	}
+
+	res, err := cs.data.Add(uint(userID), newContent)
+	if err != nil {
+		msg := ""
+		if strings.Contains(err.Error(), "not found") {
+			msg = "user not found"
+		} else {
+			msg = "unable to process the data"
+		}
+		return content.Core{}, errors.New(msg)
+	}
+	res.UserID = uint(userID)
+
+	return res, nil
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
