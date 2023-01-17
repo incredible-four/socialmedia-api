129b3fb7b9a54788e0bbc3b2e686b0f6f394ce20
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-01-17 10:26:52 +0700

    feat : Add Content in model

---
+++ features/content/data/query.go
@@ -0,0 +1,30 @@
+package data
+
+import (
+	"incrediblefour/features/content"
+
+	"gorm.io/gorm"
+)
+
+type contentData struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) content.ContentData {
+	return &contentData{
+		db: db,
+	}
+}
+
+func (cd *contentData) Add(userID uint, newContent content.Core) (content.Core, error) {
+	cnv := CoreToData(newContent)
+	cnv.UserID = uint(userID)
+	err := cd.db.Create(&cnv).Error
+	if err != nil {
+		return content.Core{}, err
+	}
+
+	newContent.ID = cnv.ID
+
+	return newContent, nil
+}
