632052adc14d76a8c2b0bdd6c848f81bbf1b3e4b
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-01-17 10:05:14 +0700

    add : Data to Core and Core to Data func in content model

--- features/content/data/model.go
+++ features/content/data/model.go
@@ -1,10 +1,30 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"incrediblefour/features/content"
 
-type Content struct {
+	"gorm.io/gorm"
+)
+
+type Contents struct {
 	gorm.Model
 	Image   string
 	Caption string
 	UserID  uint
+}
+
+func ToCore(data Contents) content.Core {
+	return content.Core{
+		ID:      data.ID,
+		Image:   data.Image,
+		Caption: data.Caption,
+	}
+}
+
+func CoreToData(data content.Core) Contents {
+	return Contents{
+		Model:   gorm.Model{ID: data.ID},
+		Image:   data.Image,
+		Caption: data.Caption,
+	}
 }
